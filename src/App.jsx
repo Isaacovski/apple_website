d14a5fe357e1f65dfa5b7426a0d4da98f8a73757
Isaacovski <codebiisaac@gmail.com>
2024-06-24 11:18:49 -0300

    Navbar bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navabr"
+import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 
--- src/components/Navabr.jsx
+++
@@ -1,7 +0,0 @@
-const Navabr = () => {
-  return (
-    <div>Navabr</div>
-  )
-}
-
-export default Navabr
